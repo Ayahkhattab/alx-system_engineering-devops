2cb6362d59f5d434752e43913a6bdbd98e29e188
Ayahkhattab <ayaahkhattab2017@gmail.com>
2024-05-08 19:03:37 -0700

    0

---
+++ 0x18-webstack_monitoring/0-sign_up_datadog.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Script to sign up for Datadog, install agent, and create application key
+
+# Commands to sign up for Datadog, install agent, and create application key
+# Remember to replace <409016-lb-01100.26.174.133> and <YOUR_APPLICATION_KEY> with actual keys
